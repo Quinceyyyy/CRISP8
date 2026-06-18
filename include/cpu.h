8ce0ff9a540cbb99cf525671d86f43bcb71d2fff
Frederick Jeffcock <frederick.jeffcock@epitech.eu>
2026-06-18 17:46:18 +0200

    build(Headers): add cpu header and utils header

---
+++ include/cpu.h
@@ -0,0 +1,39 @@
+
+#ifndef CPU_H
+    #define CPU_H
+
+    #include <stdint.h>
+    #include <stdbool.h>
+
+    #define RAM_MEMORY 4096
+    #define MAX_REGISTERS 16
+    #define STACK_SIZE 16
+    #define SCREEN_SIZE (64 * 32) // Per specs
+    #define MAX_INPUTS 16
+
+    #define ROM_START_ADDRESS 0x200 // 512 bytes
+    #define ROM_END_ADDRESS  0xFFF //4095 bytes
+
+typedef struct cpu {
+    uint8_t memory[RAM_MEMORY];
+    uint8_t delay_timer; 
+    uint8_t sound_timer;
+    uint16_t pc;
+    uint8_t sp;
+    uint8_t display_buffer[SCREEN_SIZE];
+    uint8_t input_pad[MAX_INPUTS];
+    uint16_t stack[STACK_SIZE];
+    uint8_t v_registers[MAX_REGISTERS]; // V0 -> VF (VF is splecial, it holds a flag for the carry-out for ADD, MULT etc)
+    uint16_t index_register; // I : holds the 12bit register address.
+    bool halted;
+} Cpu;
+
+// INITS
+
+int run_cpu(int argc, char *argv[]);
+
+
+uint16_t fetch_opcode(Cpu *cpu);
+int decode_opcode(Cpu *cpu, uint16_t opcode);
+
+#endif
---
+++ include/utils.h
@@ -0,0 +1,11 @@
+
+#ifndef UTILS_H
+    #define UTILS_H
+
+    typedef struct cpu Cpu;
+
+int read_rom(Cpu *cpu, char *rom_file);
+
+void print_memory(Cpu *cpu, long fsize);
+
+#endif
