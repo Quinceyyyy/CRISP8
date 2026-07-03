7116f64a6c4768f10bcd7a4fa6c7a72f19262fc6
Quinceyyyy <frederick.jeffcock@epitech.eu>
2026-07-03 15:49:57 +0200

    build(Env): add flake.nix for T480

--- flake.nix
+++ flake.nix
@@ -1,16 +1,13 @@
 {
   description = "Flake for CRISP8";
-
   inputs = { nixpkgs.url = "github:nixos/nixpkgs/nixos-25.05"; };
-
   outputs = { self, nixpkgs, ... }:
     let
       system = "x86_64-linux";
       pkgs = import nixpkgs { inherit system; };
     in {
       devShells.${system} = {
-
-        CRISP8 = pkgs.mkShell {
+        default = pkgs.mkShell {
           packages = with pkgs; [
             clang-tools
             gnumake
@@ -19,11 +16,13 @@
             gdb
             gcc
             inetutils
+            raylib
           ];
-
           shellHook = ''
             echo "Using C devShell for CRISP8"
             exec zsh -i
           '';
         };
+      };
     };
+}
